036713840cb5116ecb7435b90a07a9fd3f7695c0
Hareeswaran O S <65818462+Harepitlord@users.noreply.github.com>
2022-07-18 12:42:44 +0530

    Resolving Merge Conflicts

--- src/main/java/com/striim/BasicFileAdapters/query/QueryEngine.java
+++ src/main/java/com/striim/BasicFileAdapters/query/QueryEngine.java
@@ -1,8 +1,84 @@
+//package com.striim.BasicFileAdapters.query;
+//
+//import com.striim.BasicFileAdapters.database.*;
+//
+//import java.util.*;
+//import java.util.function.Predicate;
+//import java.util.stream.Collectors;
+//
+//public class QueryEngine {
+//    private final Scanner sc;
+//    private final InMemoryDatabase database;
+//    private ArrayList<String> fetchColumnsSet;
+//
+//    private Predicate<DataRecord> query;
+//
+//    public QueryEngine(Scanner sc, InMemoryDatabase databaseObj) {
+//        this.database = databaseObj;
+//        this.sc = sc;
+//    }
+//
+//    public void generateQueries(){
+//
+//        System.out.println("Enter the queries end with ',' to add more constraint and ';' to end the query");
+//        System.out.println("eg : col > val,col < val;");
+//        boolean another = true;
+//        do {
+//            String input = sc.nextLine();
+//            if(input.length()==0 || input.equals(";"))
+//                return;
+//            if(input.endsWith(";")) {
+//                another = false;
+//                input = input.substring(0,input.length()-1);
+//            }
+//            String[] constraints = input.split(",");
+//            for(String constraint : constraints) {
+//                if(query == null)
+//                    query = FilterFactory.getFilter(constraint);
+//                else {
+//                    Predicate<DataRecord> temp = FilterFactory.getFilter(constraint);
+//                    if(temp != null)
+//                        query = query.and(temp);
+//                }
+//            }
+//        }while(another);
+//        fetchColumns();
+//    }
+//
+//    public ArrayList<DataRecord> queryData() {
+//        if(query==null || fetchColumnsSet.get(0).equals("All") )
+//            return database.getDataObjArray();
+//        return (ArrayList<DataRecord>) database.getDataObjArray().stream().filter(query)
+//                .map(dataRecord -> dataRecord.fetchColumn(fetchColumnsSet))
+//                .collect(Collectors.toList());
+//    }
+//
+//    public void fetchColumns(){
+//        Map<String,String> record=database.getDataObjArray().get(0).getRecord();
+//        Set<String> keySet=record.keySet();
+//        System.out.println("Enter the names of the columns you want to fetch.Enter End when you want to end.For all columns enter end");
+//        System.out.println("All "+keySet);
+//        fetchColumnsSet = new ArrayList<>();
+//        String colName="Start";
+//        while(!colName.equals("END")){
+//            colName=sc.nextLine().toUpperCase();
+//            fetchColumnsSet.add(colName);
+//        }
+//        if(fetchColumnsSet.size()<=1)
+//            fetchColumnsSet.addAll(keySet);
+//        fetchColumnsSet.remove(fetchColumnsSet.size()-1);
+//    }
+//
+//}
 package com.striim.BasicFileAdapters.query;
 
-import com.striim.BasicFileAdapters.database.*;
+import com.striim.BasicFileAdapters.database.DataRecord;
+import com.striim.BasicFileAdapters.database.InMemoryDatabase;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.Set;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -56,13 +132,25 @@ public class QueryEngine {
     public void fetchColumns(){
         Map<String,String> record=database.getDataObjArray().get(0).getRecord();
         Set<String> keySet=record.keySet();
-        System.out.println("Enter the names of the columns you want to fetch.Enter End when you want to end.For all columns enter end");
-        System.out.println("All "+keySet);
+        System.out.println("Enter the names of the columns you want to fetch.Enter End when you want to end.For All Columns enter End.");
+        System.out.println(keySet);
         fetchColumnsSet = new ArrayList<>();
         String colName="Start";
-        while(!colName.equals("END")){
-            colName=sc.nextLine().toUpperCase();
-            fetchColumnsSet.add(colName);
+        while(! colName.equalsIgnoreCase("END")){
+            colName=sc.nextLine().trim();
+            boolean colfound=false;
+            for(String key : keySet){
+                if (key.equals(colName) || colName.equalsIgnoreCase("END")) {
+                    colfound = true;
+                    break;
+                }
+            }
+            if(colfound) {
+                fetchColumnsSet.add(colName);
+            }
+            else{
+                System.out.println("Enter the column name correctly !!!");
+            }
         }
         if(fetchColumnsSet.size()<=1)
             fetchColumnsSet.addAll(keySet);
