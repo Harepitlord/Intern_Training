606a01fca56f9f9b729b7a206049624a1c5062b7
Aathithya M J <aathithya.mahesh@striim.com>
2022-08-03 17:29:12 +0530

    Included QueryEngineTest

---
+++ src/test/java/com/striim/UnitTests/AddReaderTest.java
@@ -0,0 +1,28 @@
+package com.striim.UnitTests;
+
+import com.striim.BasicFileAdapters.reader.Reader;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class AddReaderTest {
+
+    private ArrayList<Reader> readers;
+
+    @BeforeAll
+    void readersSetup(){
+
+    }
+
+    @BeforeEach
+    void init(){
+
+    }
+
+    @Test
+    void addReader() {
+    }
+
+}
---
+++ src/test/java/com/striim/UnitTests/AddWriterTest.java
@@ -0,0 +1,27 @@
+package com.striim.UnitTests;
+
+import com.striim.BasicFileAdapters.writer.Writer;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+class AddWriterTest {
+
+    private ArrayList<Writer> writers;
+
+    @BeforeAll
+    void writersSetup(){
+
+    }
+
+    @BeforeEach
+    void init(){
+
+    }
+
+    @Test
+    void addWriter() {
+    }
+}
